c34390156dca4f4b18ea88cc031f600ac7d03653
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2023-06-14 20:37:41 +0530

    +A

--- Strings.cpp
+++ Strings.cpp
@@ -5,9 +5,12 @@
 #include"string.hpp"
 int main()
 {
-	V::String str("Hello world");
-	const char* ptr = str();
-	printf("%s\n", ptr);
+	V::String str("Hello world, ");
+	V::String str1("Hello Universe!");
+	V::String str2 = "Hello Galaxy!";
+	V::String str3=str + str1;
+	printf("words : %ud", str3.count_words());
+	printf("%s : %s\n", str3(),str.b_is_heap_allocated()?"heap Allocated":"stack allocated!");
 };
 
 
--- string.hpp
+++ string.hpp
@@ -1,16 +1,46 @@
 #ifndef MACRO_STRING
 #define MACRO_STRING
+#include<stdlib.h>
 namespace V
 {
 
 class String
 {
 public:
-	String(const char* str) :data((char*)str) {};
-	~String() { if (data)delete[]data; };
+	String(const char* str) :data((char*)str), b_allocated(0) {
+		
+		while (data[length] != '\0')++length;
+	};
+	~String() {if(b_allocated) delete [] data; };
 	const char* operator()(){ return this->data; };
+	const char* operator+(const String& rhs)
+	{
+		
+		auto total_len{ this->length + rhs.length };
+		char* new_char = new char[total_len];
+		int i = 0;
+		for (; i < this->length; ++i)new_char[i] = this->data[i];
+		for (int j = 0; j <= rhs.length; ++i, ++j)new_char[i] = rhs.data[j];
+		return new_char;
+	};
+
+public:
+	auto get_length()const { return this->length; };
+	auto b_is_heap_allocated()const { return this->b_allocated; };
+	int count_words() {
+	for (int i = 0; i < length; ++i)
+	{
+		if (data[i] == ' ' && data[i - 1]==' ')
+		{
+			++words;
+		}
+	}
+	return words;
+	};
 private:
-	unsigned long int length{};
+	unsigned long int words{};
+	bool b_allocated;
+	unsigned int length{};
 	char* data{ nullptr };
 };
 }
